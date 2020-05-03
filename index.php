435245c06588d8079eeb2309c285cc9c05974c04
Lê Anh Vũ <leanhvu>
2020-05-04 06:20:26 +0700

    web php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  echo "hello guys";
+  echo "hello guys";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>my first PHP file</title>
+</head>
+
+<body>
+  <h1><?php echo "This is content inside PHP code"; ?></h1>
+</body>
+
+</html>
